1ded0e4446b30d0d79cdedcd77d2b9732ac5a7ec
telontelvarost <telontelvarost@gmail.com>
2024-02-09 22:21:43 -0500

    UDOC-0 - Move settings to mod helper

--- src/main/java/com/github/telvarost/hudtweaks/ModHelper.java
+++ src/main/java/com/github/telvarost/hudtweaks/ModHelper.java
@@ -4,6 +4,12 @@ public class ModHelper {
 
     public static class ModHelperFields {
 
-        public static boolean isTakingIsometricScreenshot = false;
+        public static Integer isomScale = 16;
+
+        public static Integer hugeWidth = 7680;
+
+        public static Integer hugeHeight = 2240;
+
+        public static Boolean isTakingIsometricScreenshot = false;
     }
 }
