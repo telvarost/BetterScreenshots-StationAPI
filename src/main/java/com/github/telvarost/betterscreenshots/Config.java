c9dc03a19e6e87bfc0245bd8aaca4f84973eb955
telvarost <telontelvarost@gmail.com>
2024-10-20 21:40:56 -0400

    Migrate to GCAPI3

--- src/main/java/com/github/telvarost/betterscreenshots/Config.java
+++ src/main/java/com/github/telvarost/betterscreenshots/Config.java
@@ -1,48 +1,57 @@
 package com.github.telvarost.betterscreenshots;
 
-import blue.endless.jankson.Comment;
-import net.glasslauncher.mods.api.gcapi.api.ConfigName;
-import net.glasslauncher.mods.api.gcapi.api.GConfig;
-import net.glasslauncher.mods.api.gcapi.api.MaxLength;
+import net.glasslauncher.mods.gcapi3.api.*;
 
 public class Config {
 
-    @GConfig(value = "config", visibleName = "BetterScreenshots")
+    @ConfigRoot(value = "config", visibleName = "BetterScreenshots")
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
-        @ConfigName("Add Basic Screenshots To Clipboard")
+        @ConfigEntry(name = "Add Basic Screenshots To Clipboard")
         public Boolean addBasicScreenshotsToClipboard = true;
 
-        @ConfigName("Custom Screenshot Height In Pixels")
-        @MaxLength(36863)
-        @Comment("Default Value: 2240")
+        @ConfigEntry(
+                name = "Custom Screenshot Height In Pixels",
+                description = "Default Value: 2240",
+                maxLength = 36863
+        )
         public Integer customResolutionPhotoHeight = 2240;
 
-        @ConfigName("Custom Screenshot Width In Pixels")
-        @MaxLength(36863)
-        @Comment("Default Value: 7680")
+        @ConfigEntry(
+                name = "Custom Screenshot Width In Pixels",
+                description = "Default Value: 7680",
+                maxLength = 36863
+        )
         public Integer customResolutionPhotoWidth = 7680;
 
-        @ConfigName("Disable Rendering Nether Bedrock")
-        @Comment("Reload world for changes to take effect")
+        @ConfigEntry(
+                name = "Disable Rendering Nether Bedrock",
+                description = "Reload world for changes to take effect"
+        )
         public Boolean disableRenderingNetherBedrock = false;
 
-        @ConfigName("Isometric Screenshot Resolution")
-        @MaxLength(255)
-        @Comment("Default Value: 8")
+        @ConfigEntry(
+                name = "Isometric Screenshot Resolution",
+                description = "Default Value: 8",
+                maxLength = 255
+        )
         public Integer isometricPhotoScale = 8;
 
-        @ConfigName("Isometric Screenshot Rotation")
+        @ConfigEntry(name = "Isometric Screenshot Rotation")
         public IsometricAngleEnum isometricPhotoRotation = IsometricAngleEnum.ANGLE_0_DEGREES;
 
-        @ConfigName("Isometric Screenshot Rotation Offset")
-        @MaxLength(90)
-        @Comment("Zero degrees recommended for best angle")
+        @ConfigEntry(
+                name = "Isometric Screenshot Rotation Offset",
+                description = "Zero degrees recommended for best angle",
+                maxLength = 90
+        )
         public Integer isometricPhotoRotationOffset = 0;
 
-        @ConfigName("Mirror Isometric Screenshot")
-        @Comment("Flips the isometric screenshot horizontally")
+        @ConfigEntry(
+                name = "Mirror Isometric Screenshot",
+                description = "Flips the isometric screenshot horizontally"
+        )
         public Boolean mirrorIsometricScreenshot = false;
     }
 }
--- src/main/java/com/github/telvarost/betterscreenshots/IsometricAngleEnumFactory.java
+++ src/main/java/com/github/telvarost/betterscreenshots/IsometricAngleEnumFactory.java
@@ -1,21 +1,19 @@
 package com.github.telvarost.betterscreenshots;
 
-import blue.endless.jankson.JsonElement;
-import blue.endless.jankson.JsonPrimitive;
 import com.google.common.collect.ImmutableMap;
-import net.glasslauncher.mods.api.gcapi.api.ConfigFactoryProvider;
-import net.glasslauncher.mods.api.gcapi.api.MaxLength;
-import net.glasslauncher.mods.api.gcapi.impl.NonFunction;
-import net.glasslauncher.mods.api.gcapi.impl.config.ConfigEntry;
-import net.glasslauncher.mods.api.gcapi.impl.config.entry.EnumConfigEntry;
+import net.glasslauncher.mods.gcapi3.api.*;
+import net.glasslauncher.mods.gcapi3.impl.SeptFunction;
+import net.glasslauncher.mods.gcapi3.impl.object.ConfigEntryHandler;
+import net.glasslauncher.mods.gcapi3.impl.object.entry.EnumConfigEntryHandler;
 
 import java.lang.reflect.*;
 import java.util.function.*;
 
 public class IsometricAngleEnumFactory implements ConfigFactoryProvider {
+
     @Override
-    public void provideLoadFactories(ImmutableMap.Builder<Type, NonFunction<String, String, String, Field, Object, Boolean, Object, Object, MaxLength, ConfigEntry<?>>> immutableBuilder) {
-        immutableBuilder.put(IsometricAngleEnum.class, ((id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum, maxLength) ->
+    public void provideLoadFactories(ImmutableMap.Builder<Type, SeptFunction<String, ConfigEntry, Field, Object, Boolean, Object, Object, ConfigEntryHandler<?>>> immutableBuilder) {
+        immutableBuilder.put(IsometricAngleEnum.class, ((id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrOrdinal, defaultEnum) ->
         {
             int enumOrdinal;
             if(enumOrOrdinal instanceof Integer ordinal) {
@@ -24,17 +22,12 @@ public class IsometricAngleEnumFactory implements ConfigFactoryProvider {
             else {
                 enumOrdinal = ((IsometricAngleEnum) enumOrOrdinal).ordinal();
             }
-            return new EnumConfigEntry<IsometricAngleEnum>(id, name, description, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((IsometricAngleEnum) defaultEnum).ordinal(), IsometricAngleEnum.class);
+            return new EnumConfigEntryHandler<IsometricAngleEnum>(id, configEntry, parentField, parentObject, isMultiplayerSynced, enumOrdinal, ((IsometricAngleEnum) defaultEnum).ordinal(), IsometricAngleEnum.class);
         }));
     }
 
     @Override
-    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, JsonElement>> immutableBuilder) {
-        immutableBuilder.put(IsometricAngleEnum.class, enumEntry -> new JsonPrimitive(((IsometricAngleEnum) enumEntry).ordinal()));
-    }
-
-    @Override
-    public void provideLoadTypeAdapterFactories(@SuppressWarnings("rawtypes") ImmutableMap.Builder<Type, Class> immutableBuilder) {
-        immutableBuilder.put(IsometricAngleEnum.class, Integer.class);
+    public void provideSaveFactories(ImmutableMap.Builder<Type, Function<Object, Object>> immutableBuilder) {
+        immutableBuilder.put(IsometricAngleEnum.class, enumEntry -> enumEntry);
     }
 }
